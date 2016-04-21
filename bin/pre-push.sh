1f782d6637e8bfd1f093030d68dc0a860d842afc
luics <luics.king@gmail.com>
2016-04-21 22:56:00 +0800

    add [all] prepare pre-push

--- bin/pre-push.sh
+++ bin/pre-push.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-echo "$1 $2"
+branch_regex="^(master|dev|android|ios|jsfm|html5|doc|website|example|test|all)(-(bugfix|feature|hotfix)-.+)?$"
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+if ! (echo $branch_name | grep -iqE "$branch_regex"); then
+    echo "\nERROR: branch name format is not correct! 
+See https://github.com/alibaba/weex/blob/dev/CONTRIBUTING.md#branch-name\n"
+    exit 1
+fi
 
 exit 1
