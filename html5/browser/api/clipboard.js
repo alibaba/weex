20dd9a7097852224696a23c0cd0e41c1868aff47
行久 <yiyuan.zhangyy@alibaba-inc.com>
2016-08-03 13:23:09 +0800

    [html5] code style [android] comment.

--- html5/browser/api/clipboard.js
+++ html5/browser/api/clipboard.js
@@ -1,63 +1,60 @@
 'use strict'
 
-
 /**
 
 AUCTION:
 taskQueue
 Clipboard.setString()  NOW not works, facing to user-act lose of taskQueue.
 
-works in Chrome Firefox Opera. but not in Safari. 
+works in Chrome Firefox Opera. but not in Safari.
 @see https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand#Browser_compatibility
 
 Clipboard.getString() unimplemented. There is no easy way to do paste from clipboard to js variable.
 
-So look out your app behavior, when downgrade to html5 render. 
+So look out your app behavior, when downgrade to html5 render.
 Any idea is welcome.
 **/
 
-const SUCCESS = 'success'
-const FAILED = 'failed'
-const WEEX_CLIPBOARD_ID = "__weex_clipboard_id__";
+const WEEX_CLIPBOARD_ID = '__weex_clipboard_id__'
 
 const clipboard = {
 
   getString: function (callbackId) {
     // not supported in html5
-    console.log("clipboard.getString() is not supported now.");
+    console.log('clipboard.getString() is not supported now.')
   },
 
   setString: function (text) {
     // not support safari
-    if(typeof text === 'string' && '' != text) {
-      var tempInput = element();
-      tempInput.value = text;
+    if (typeof text === 'string' && text !== '') {
+      const tempInput = element()
+      tempInput.value = text
 
-      tempInput.select();
-      document.execCommand('copy');
+      tempInput.select()
+      document.execCommand('copy')
       // var out = document.execCommand('copy');
       // console.log("execCommand out is " + out);
-      tempInput.value = "";
-      tempInput.blur(); 
-    }else {
-      console.log("only support string input now");
+      tempInput.value = ''
+      tempInput.blur()
+    }
+    else {
+      console.log('only support string input now')
     }
   }
 
 }
 
-function element() {
-  var tempInput = document.getElementById(WEEX_CLIPBOARD_ID);
-  if(tempInput == undefined) {
-      tempInput = document.createElement("input");
-      tempInput.setAttribute("id", WEEX_CLIPBOARD_ID);
-      tempInput.style.cssText = "height:1px;width:1px;border:none;"
-      // tempInput.style.cssText = "height:40px;width:300px;border:solid;"
-      document.body.appendChild(tempInput);
+function element () {
+  let tempInput = document.getElementById(WEEX_CLIPBOARD_ID)
+  if (tempInput === undefined) {
+    tempInput = document.createElement('input')
+    tempInput.setAttribute('id', WEEX_CLIPBOARD_ID)
+    tempInput.style.cssText = 'height:1px;width:1px;border:none;'
+    // tempInput.style.cssText = "height:40px;width:300px;border:solid;"
+    document.body.appendChild(tempInput)
   }
-  return tempInput;
+  return tempInput
 }
-
 
 clipboard._meta = {
   clipboard: [{
@@ -68,6 +65,5 @@ clipboard._meta = {
     args: ['string']
   }]
 }
-
 
 module.exports = clipboard
