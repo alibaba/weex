2eaa64c15f2c0ea0def9b027ace77e4786048013
kenshin <xin.chen@welines.cn>
2016-12-27 01:12:29 +0800

    change line print to english

--- ios/sdk/WeexSDK/Sources/Utility/WXLog.m
+++ ios/sdk/WeexSDK/Sources/Utility/WXLog.m
@@ -42,7 +42,7 @@
 #define XCODE_COLORS_RESET      XCODE_COLORS_ESCAPE_SEQ ";"  // Clear any foreground or background color
 
 #ifdef DEBUG
-#define WXInnerLog(format, ...) printf("[%s] %s [第%d行] %s\n", __TIME__, __FUNCTION__, __LINE__, [[NSString stringWithFormat:format, ## __VA_ARGS__] UTF8String]);
+#define WXInnerLog(format, ...) printf("[%s] %s [line: %d] %s\n", __TIME__, __FUNCTION__, __LINE__, [[NSString stringWithFormat:format, ## __VA_ARGS__] UTF8String]);
 #else
 #define WXInnerLog(format, ...)
 #endif
