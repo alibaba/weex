be9b425c51bc0a5fb56ec88efabd14c8544154a9
sospartan zheng <sospartan@apache.org>
2016-12-28 18:23:57 +0800

    test toggle

--- ios/sdk/WeexSDK/Sources/Utility/WXLog.m
+++ ios/sdk/WeexSDK/Sources/Utility/WXLog.m
@@ -6,6 +6,7 @@
  * For the full copyright and license information,please view the LICENSE file in the root directory of this source tree.
  */
 
+
 #import "WXLog.h"
 #import "WXUtility.h"
 #import "WXAssert.h"
